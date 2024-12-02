115d27aae958fde75ac42b19aef835dba5310edf
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2024-12-02 19:11:57 +0100

    Update _START.bat

--- _START.bat
+++ _START.bat
@@ -1,2 +1,15 @@
+@ECHO OFF
+
+ECHO : Updating :
+CD update
+..\python_embeded\python.exe .\update.py ..\ComfyUI\ --stable
+if exist update_new.py (
+  move /y update_new.py update.py
+  echo Running updater again since it got updated.
+  ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --skip_self_update --stable
+)
+
+ECHO : Staring :
+CD ..\
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --output-directory R:\ComfyUI\
 pause
