c1141de314ee8757c2d9c6f34263fb3af8ab341f
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2024-12-22 11:28:08 +0100

    Don't update if disabled.

--- _START.bat
+++ _START.bat
@@ -1,12 +1,16 @@
 @ECHO OFF
 
 ECHO : Updating :
-CD update
-..\python_embeded\python.exe .\update.py ..\ComfyUI\ --stable
-if exist update_new.py (
-  move /y update_new.py update.py
-  echo Running updater again since it got updated.
-  ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --skip_self_update --stable
+if exist ComfyUI\.git\ (
+  CD update
+  ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --stable
+  if exist update_new.py (
+    move /y update_new.py update.py
+    echo Running updater again since it got updated.
+    ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --skip_self_update --stable
+  )
+) else (
+  echo Updating disabled.
 )
 
 ECHO : Staring :
