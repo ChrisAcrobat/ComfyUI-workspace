4c45e255fbc0ace3faa945bc568e16516f401510
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2024-12-22 13:42:19 +0100

    Fixed start up.

--- _START.bat
+++ _START.bat
@@ -9,11 +9,11 @@ if exist ComfyUI\.git\ (
     echo Running updater again since it got updated.
     ..\python_embeded\python.exe .\update.py ..\ComfyUI\ --skip_self_update --stable
   )
+  CD ..\
 ) else (
   echo Updating disabled.
 )
 
 ECHO : Staring :
-CD ..\
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --output-directory R:\ComfyUI\
 pause
